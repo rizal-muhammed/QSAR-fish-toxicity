1d8576b71ffc8be3805e2c7ce76044f3ef580db9
rizal-muhammed <rizalmuhammedmp@gmail.com>
2023-11-29 19:05:19 +0530

    main updated.

--- main.py
+++ main.py
@@ -7,65 +7,65 @@ from QSARFishToxicity.pipeline.stage_05_data_preprocessing_training import DataP
 from QSARFishToxicity.pipeline.stage_06_model_training import ModelTrainingPipeline
 from QSARFishToxicity.pipeline.stage_07_model_prediction import PredictionPipeline
 
-# STAGE_NAME = f"""Data Ingestion"""
-# try:
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
-#     data_ingestion = DataIngestionPipeline()
-#     data_ingestion.main()
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed... <<<<<<<\n\n""")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+STAGE_NAME = f"""Data Ingestion"""
+try:
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
+    data_ingestion = DataIngestionPipeline()
+    data_ingestion.main()
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed... <<<<<<<\n\n""")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME = f"""Data Validation Training"""
-# try:
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
-#     data_validation_training = DataValidationTrainingPipeline()
-#     data_validation_training.main()
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed... <<<<<<<\n\n""")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+STAGE_NAME = f"""Data Validation Training"""
+try:
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
+    data_validation_training = DataValidationTrainingPipeline()
+    data_validation_training.main()
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed... <<<<<<<\n\n""")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME = f"""Data Transformation Training"""
-# try:
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
-#     data_transformation_training = DataTransformationTrainingPipeline()
-#     data_transformation_training.main()
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed... <<<<<<<\n\n""")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+STAGE_NAME = f"""Data Transformation Training"""
+try:
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
+    data_transformation_training = DataTransformationTrainingPipeline()
+    data_transformation_training.main()
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed... <<<<<<<\n\n""")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME = f"""Database Operations Training"""
-# try:
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
-#     data_insertion_training = DatabaseOperationsTrainingPipeline()
-#     data_insertion_training.main()
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed, and data exported as csv for training <<<<<<<\n\n""")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+STAGE_NAME = f"""Database Operations Training"""
+try:
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
+    data_insertion_training = DatabaseOperationsTrainingPipeline()
+    data_insertion_training.main()
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed, and data exported as csv for training <<<<<<<\n\n""")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME = f"""Data Pre-Processing Training"""
-# try:
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
-#     data_preprocessing_training = DataPreProcessingTrainingPipeline()
-#     data_preprocessing_training.main()
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed, and data exported as csv for model building <<<<<<<\n\n""")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+STAGE_NAME = f"""Data Pre-Processing Training"""
+try:
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
+    data_preprocessing_training = DataPreProcessingTrainingPipeline()
+    data_preprocessing_training.main()
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed, and data exported as csv for model building <<<<<<<\n\n""")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
-# STAGE_NAME = f"""Model Training"""
-# try:
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
-#     model_training = ModelTrainingPipeline()
-#     model_training.main()
-#     logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed. <<<<<<<\n\n""")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+STAGE_NAME = f"""Model Training"""
+try:
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} started... <<<<<<<""")
+    model_training = ModelTrainingPipeline()
+    model_training.main()
+    logger.info(f""">>>>>>> Stage {STAGE_NAME} Completed. <<<<<<<\n\n""")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 STAGE_NAME = f"""Prediction"""
 try:

9ce653b5be382bd1f819942304788eebc5447bb2
rizal-muhammed <rizalmuhammedmp@gmail.com>
2023-11-29 11:34:10 +0530

    Utility functions added.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "QSAR-fish-toxicity"
+AUTHOR_USER_NAME = "rizal-muhammed"
+SRC_REPO = "QSARFishToxicity"
+AUTHOR_EMAIL = "rizalmuhammedm.p@gmail.com"
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description=f"""Prediction of LC50 value using Quantitative structure 
+    activity relationship models (QSAR models) with Machine Learning.""",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
---
+++ src/PredictiveMaintenance/config/configuration.py
@@ -0,0 +1,2 @@
+from pathlib import Path
+from PredictiveMaintenance.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH, SECRETS_FILE_PATH
